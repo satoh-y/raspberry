0d6dee03a43290dd72115dc77dbc2eebe2901df7
satoh <saton@Solomon.local>
2015-07-19 22:13:02 +0900

    no message

---
+++ exec/Sshutdown.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+GPIO=6
+TIMER=3
+
+echo "$GPIO" > /sys/class/gpio/export
+echo "in" > /sys/class/gpio/gpio$GPIO/direction
+echo "low" > /sys/class/gpio/gpio$GPIO/direction
+
+cnt=0
+
+while [ $cnt -lt $TIMER ] ; do
+  sensor=`cat /sys/class/gpio/gpio$GPIO/value`
+  if [ "$sensor" -eq "1" ] ; then
+    cnt=`expr $cnt + 1`
+  else
+    cnt=0
+  fi
+  sleep 1
+done
+
+
+sync
+sync
+
+shutdown -h now
--- exec/shutdown.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sync
-sync
-shutdown -h now
